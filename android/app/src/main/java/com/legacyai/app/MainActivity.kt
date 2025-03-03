5129a3c5b0f4981d4ea33397b0927553ef440607
peterevans10 <peterxevans10@gmail.com>
2025-03-02 19:49:13 -0800

    proj running with boilerplate sign up and superwall

--- android/app/src/main/java/com/legacyai/app/MainActivity.kt
+++ android/app/src/main/java/com/legacyai/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.cqjack.expoappboilerplate
+package com.legacyai.app
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/legacyai/app/MainApplication.kt
+++ android/app/src/main/java/com/legacyai/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.cqjack.expoappboilerplate
+package com.legacyai.app
 
 import android.app.Application
 import android.content.res.Configuration
